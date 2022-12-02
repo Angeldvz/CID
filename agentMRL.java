87fd18e64a27b7215f60ef693865f0886231a7b5
Angel David <79676039+Angeldvz@users.noreply.github.com>
2022-12-01 20:38:22 -0600

    MLRAgent
    Multiple Regretion Linear Agent

---
+++ agentMRL.java
@@ -0,0 +1,46 @@
+//javac -cp lib\jade.jar -d classes C:\CID\*.java
+//java -cp lib\jade.jar;classes jade.Boot -gui -agents MLR:mLinearMatrix.agentMRL
+package mLinearMatrix;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+
+
+public class agentMRL extends Agent {
+
+    private double predictX1 = 0.0;
+    private double predictX2 = 0.0;
+    private double predictX3 = 0.0;
+    private static final matrixMLR train = new matrixMLR();
+    
+    private static double bethas [] = new double[4];
+    protected void setup() {
+        System.out.println("Agent " + getLocalName() + " started.");
+
+        //Object[] args = getArguments();
+        //if (args != null && args.length > 0) {
+            addBehaviour(new Train());
+            predictX1 = 100000;//Float.parseFloat((String) args[0]);
+            predictX2 = 100000;
+            predictX3 = 100000;
+            System.out.println("x1 to predict = " + predictX1);
+            System.out.println("x2 to predict = " + predictX2);
+            System.out.println("x3 to predict = " + predictX3);
+       // }
+
+    }
+
+    private class Train extends OneShotBehaviour{
+        public void action(){
+            bethas=train.train();
+        }
+        public int onEnd() {
+            StringBuilder s = new StringBuilder();
+            s.append(String.format("%.2f + %.2fx1+%.2fx2+ %.2fx3", bethas[0], bethas[1], bethas[2], bethas[3]));
+            System.out.print("\nSimple linear regression equation: " + s.toString());
+            double predicted_y = (bethas[1] * predictX1)+(bethas[2] * predictX2)+(bethas[3] * predictX3) + bethas[0];
+            System.out.print(String.format("\nx1: %.2f | x2: %.2f | x3: %.2f |predicted y: %.2f\n", predictX1, predictX2, predictX3, predicted_y));
+            
+            return super.onEnd();
+        }
+    }
+}
